9524c8c5842f63083d2436e6b5510e8db277fd77
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-08 12:25:54 -0400

    added condition to strike completed task

--- index.view.php
+++ index.view.php
@@ -9,7 +9,13 @@
 <body>
     <ul>
         <?php foreach ($tasks as $task): ?>
-            <li><?=$task->description;?></li>
+            <li>
+                <?php if ($task->completed): ?>
+                    <s><?=$task->description;?></s>
+                <?php else: ?>
+                    <?=$task->description;?>
+                <?php endif;?>
+            </li>
         <?php endforeach;?>
     </ul>
 </body>
