513078d5d892643c5862b4e7efa6c1cb797cd06a
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-08 12:15:54 -0400

    created a list for todo items

--- index.view.php
+++ index.view.php
@@ -7,6 +7,10 @@
     <title>Document</title>
 </head>
 <body>
-
+    <ul>
+        <?php foreach ($tasks as $task): ?>
+            <li><?=$task->description;?></li>
+        <?php endforeach;?>
+    </ul>
 </body>
 </html>
